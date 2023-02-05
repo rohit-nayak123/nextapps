079d0020b3b0380dd4852a75b537dc777aa464c4
rohit-nayak123 <113372030+rohit-nayak123@users.noreply.github.com>
2023-02-05 17:56:48 +0530

    Initial commit

---
+++ components/Test.jsx
@@ -0,0 +1,4 @@
+function Practice(props) {
+  return <> now it is working properly {props.name}̥</>;
+}
+export default Practice;
---
+++ pages/index.jsx
@@ -0,0 +1,49 @@
+import Test from "../components/Test";
+// how to import conponents in next js
+// how to call a function in next js
+function index() {
+  return (
+    <>
+      <div>
+        <p>hello woeld</p>
+      </div>
+      <form action="">
+        <div>
+          <input
+            type="text"
+            name="name"
+            id="intputName "
+            placeholder="First name"
+            required
+            autoComplete="off"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="name"
+            id="intputName "
+            placeholder="Last name"
+            required
+            autoComplete="off"
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="name"
+            id="intputName "
+            placeholder="E-mail"
+            required
+            autoComplete="off"
+          />
+        </div>
+        <button type="submit" id="btn">
+          Submit
+        </button>
+      </form>
+      <Test name="baby" />
+    </>
+  );
+}
+export default index;
